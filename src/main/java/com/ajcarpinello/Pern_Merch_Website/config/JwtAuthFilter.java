e02ce3abd624f792050a33b865a974a40f86b4a4
AJ Carpinello <ajcarp81003@gmail.com>
2026-04-07 21:59:47 -0400

    config files

---
+++ src/main/java/com/ajcarpinello/Pern_Merch_Website/config/JwtAuthFilter.java
@@ -0,0 +1,4 @@
+package com.ajcarpinello.Pern_Merch_Website.config;
+
+public class JwtAuthFilter {
+}
--- src/main/java/com/ajcarpinello/Pern_Merch_Website/config/JwtUtil.java
+++ src/main/java/com/ajcarpinello/Pern_Merch_Website/config/JwtUtil.java
@@ -5,6 +5,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
@@ -38,11 +39,11 @@ public class JwtUtil {
 
     public boolean isTokenValid(String token, UserDetails userDetails) {
         String username = extractUsername(token);
-        return username.equals(userDetails.getUername()) && !isTokenExpired(token);
+        return username.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
 
     private boolean isTokenExpired(String token) {
-        return extractClaims(token).getExpiration.before(new Date());
+        return extractClaims(token).getExpiration().before(new Date());
     }
 
     private Claims extractClaims(String token) {
